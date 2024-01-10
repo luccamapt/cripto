92b5fae80f2c91051eb5907100bf3bb6e343c5c2
root <root@yLLz-Nitro>
2024-01-10 17:49:21 -0300

    Renomeação de variaveis e documentação

--- ecc/ecc.c
+++ ecc/ecc.c
@@ -1,4 +1,28 @@
 /* 
+INTRODUÇÃO A CRIPTOGRAFIA
+
+     .--------.
+    / .------. \
+   / /        \ \
+   | |        | |
+  _| |________| |_
+.' |_|        |_| '.
+'._____ ____ _____.'
+|     .'____'.     |
+'.__.'.'    '.'.__.'
+'.__  |  ECC |  __.'
+|   '.'.____.'.'   |
+'.____'.____.'____.'
+'.________________.'
+
+
+Guilherme Milani de Oliveira - 771016
+Lucca Marques Almeida Paes Teles - 771034
+
+
+
+SOBRE O PROGRAMA
+
 • typedef struct { int x, y; } Point;: Define uma estrutura chamada Point para representar pontos na curva elíptica.
 
 • int extended_gcd(int x, int n, int *i, int *j): Função para calcular o GCD estendido.
@@ -11,12 +35,12 @@
 
 • int main(): Função principal.
 
-• O loop while (1) lê os casos de teste até que n seja igual a zero.
+  • O loop while (1) lê os casos de teste até que n seja igual a zero.
 
-• Point result = ecc_multiply((Point){Xg, Yg}, n, a, p);: Chama a função ecc_multiply para obter o resultado.
+  • Point result = ecc_multiply((Point){Xg, Yg}, n, a, p);: Chama a função ecc_multiply para obter o resultado.
 
-• printf("%d %d\n", result.x, result.y);: Imprime as coordenadas do ponto resultante. */
-
+  • printf("%d %d\n", result.x, result.y);: Imprime as coordenadas do ponto resultante. 
+  */
 
 
 #include <stdio.h>
@@ -49,31 +73,26 @@ int mod_inverse(int x, int n) {
     return -1; // O inverso multiplicativo não existe
 }
 
-Point ecc_add(Point P, Point Q, int a, int p) {
+Point ecc_add(Point G, Point Q, int a, int p) {
   Point R;
-  if (P.x == 0 && P.y == 0)
+  if (Q.x == 0 && Q.y == 0)
+    return G;
+  else if (G.x == 0 && G.y == 0)
     return Q;
-  else if (Q.x == 0 && Q.y == 0)
-    return P;
-  else if (P.x == Q.x && P.y != Q.y) {
+  else if (Q.x == G.x && Q.y != G.y) {
     R.x = 0;
     R.y = 0;
     return R;
   }
   else {
     int lam;
-    if (P.x != Q.x)
-      lam = ((Q.y - P.y) * mod_inverse(Q.x - P.x, p)) % p;
-    else if (P.y != 0)
-      lam = ((3 * P.x * P.x + a) * mod_inverse(2 * P.y, p)) % p;
-    else {
-      R.x = 0;
-      R.y = 0;
-      return R;
-    }
+    if (Q.x != G.x || Q.y != G.y)
+      lam = ((G.y - Q.y) * mod_inverse(G.x - Q.x, p)) % p;
+    else if (Q.y != 0)
+      lam = ((3 * Q.x * Q.x + a) * mod_inverse(2 * Q.y, p)) % p;
 
-    R.x = (lam * lam - P.x - Q.x) % p;
-    R.y = (lam * (P.x - R.x) - P.y) % p;
+    R.x = (lam * lam - Q.x - G.x) % p;
+    R.y = (lam * (Q.x - R.x) - Q.y) % p;
 
     return R;
   }
@@ -82,8 +101,8 @@ Point ecc_add(Point P, Point Q, int a, int p) {
 Point ecc_multiply(Point G, int n, int a, int p) {
   Point R = {0, 0};
   for (int i = 0; i < sizeof(int) * 8; i++) {
-    if ((n & 1) == 1)
-      R = ecc_add(R, G, a, p);
+    if (n & 1)
+      R = ecc_add(G, R, a, p);
     G = ecc_add(G, G, a, p);
     n >>= 1;
   }
